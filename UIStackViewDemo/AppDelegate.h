9729ea0e504fb98b6c7997706239b93c22d10c3d
774655048@qq.com <774655048@qq.com>
2018-04-27 09:00:38 +0800

    Initial Commit

---
+++ UIStackViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIStackViewDemo
+//
+//  Created by YueWen on 2018/4/27.
+//  Copyright © 2018年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
